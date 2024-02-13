a266119496089e14f1a44140b1121836ef62e5c0
ImSmorgas <147559641+ImSmorgas@users.noreply.github.com>
2024-02-13 17:05:29 -0500

    test

--- js/landing-js/script.js
+++ js/landing-js/script.js
@@ -1,15 +1,12 @@
 $(document).ready(function () {
-    // Disable horizontal touch scrolling on mobile and tablets
-    document.addEventListener('touchmove', function (event) {
-        if (event.touches.length > 1) {
-            event.preventDefault();
-        }
-    }, { passive: false });
     // Scroll to the section with ID #2 on page load without animation
-    const section2 = document.getElementById('2');
-    if (section2) {
-        section2.scrollIntoView({ behavior: 'auto', block: 'start' });
-    }
+    window.onload = function () {
+    window.scrollTo({
+        left: window.innerWidth,
+        behavior: 'instant'  // Scroll instantly
+    });
+}
+
 
     // Background Animations
     gsap.to('#bg-c1', {
